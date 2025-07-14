38062968773a28f7e3b7305d1a81ee1878739f27
gwangho69 <ckrhkdgh22@gmail.com>
2025-07-14 20:09:57 +0900

    writing

--- LegacyFiles/Legacy/Legacy/Gwangho/MaiN.cs
+++ LegacyFiles/Legacy/Legacy/Gwangho/MaiN.cs
@@ -18,7 +18,30 @@ namespace Legacy.Gwangho
             Console.WriteLine($"캐릭터의 이름은 {name} 입니다. 맞습니까?");
             Console.WriteLine("1. 응 맞아.");
             Console.WriteLine("2. 아닌데?");
-            string choice1 = 
+            string choice1 = Console.ReadLine();
+            if (int.TryParse(choice1, out int cho1))
+            {
+                // cho1에는 변환된 정수값이 들어 있음
+                if (cho1 == 1)
+                {
+                    Console.WriteLine($"알겠습니다. 캐릭터의 이름은{name} 입니다.");
+                }
+                else if (cho1 == 2)
+                {
+                    Main();
+                }
+                else
+                {
+                    Console.WriteLine("1이나 2만 입력해 주세요.");
+                    Thread.Sleep(1500);
+                    Main();
+                }
+            }
+            else
+            {
+                Console.WriteLine("숫자로 입력해 주세요.");
+                Main();
+            }
         }
         
 
