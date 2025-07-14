b4a8fc0b01059bddb6a609d712d23dc37318c9c9
gwangho69 <ckrhkdgh22@gmail.com>
2025-07-14 19:21:03 +0900

    console

---
+++ LegacyFiles/Legacy/Legacy/Gwangho/MaiN.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legacy.Gwangho
+{
+    internal class MaiN
+    {
+        static void Main()
+        {
+            Console.Clear();
+            Console.WriteLine("스파게티르타 마을에 오신 걸 환영합니다.");
+            Console.WriteLine("캐릭터의 이름을 입력해 주십시오.");
+            string name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"캐릭터의 이름은 {name} 입니다. 맞습니까?");
+            Console.WriteLine("1. 응 맞아.");
+            Console.WriteLine("2. 아닌데?");
+            string choice1 = 
+        }
+        
+
+    }
+}
---
+++ LegacyFiles/Legacy/Legacy/Gwangho/Profile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legacy.Gwangho
+{
+    internal class Profile
+    {
+
+        static int level = 1;
+        static float atk = 10;
+        static int def = 5;
+        static int hp = 100;
+        static int gold = 1500;
+
+        static int exp = 0;
+        static int maxexp = 100;
+
+    }
+}
