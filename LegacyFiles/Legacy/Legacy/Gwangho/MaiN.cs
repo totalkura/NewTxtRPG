256d289270ce46a1650a32c8727e893dd78c1db1
gwangho69 <ckrhkdgh22@gmail.com>
2025-07-15 09:24:49 +0900

    fix

--- LegacyFiles/Legacy/Legacy/Gwangho/MaiN.cs
+++ LegacyFiles/Legacy/Legacy/Gwangho/MaiN.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Legacy.Gwangho
 {
@@ -10,40 +7,56 @@ namespace Legacy.Gwangho
     {
         static void Main()
         {
-            Console.Clear();
-            Console.WriteLine("스파게티르타 마을에 오신 걸 환영합니다.");
-            Console.WriteLine("캐릭터의 이름을 입력해 주십시오.");
-            string name = Console.ReadLine();
-            Console.Clear();
-            Console.WriteLine($"캐릭터의 이름은 {name} 입니다. 맞습니까?");
-            Console.WriteLine("1. 응 맞아.");
-            Console.WriteLine("2. 아닌데?");
-            string choice1 = Console.ReadLine();
-            if (int.TryParse(choice1, out int cho1))
+            string name;
+            string job;
+
+            while (true)
             {
-                // cho1에는 변환된 정수값이 들어 있음
-                if (cho1 == 1)
+                Console.Clear();
+                Console.WriteLine("스파게티르타 마을에 오신 걸 환영합니다.");
+                Console.WriteLine("캐릭터의 이름을 입력해 주십시오.");
+                name = Console.ReadLine();
+
+                Console.Clear();
+                Console.WriteLine($"캐릭터의 이름은 {name} 입니다. 맞습니까?");
+                Console.WriteLine("1. 응 맞아.");
+                Console.WriteLine("2. 아닌데?");
+                string choice1 = Console.ReadLine();
+
+                if (int.TryParse(choice1, out int cho1))
                 {
-                    Console.WriteLine($"알겠습니다. 캐릭터의 이름은{name} 입니다.");
-                }
-                else if (cho1 == 2)
-                {
-                    Main();
+                    if (cho1 == 1)
+                    {
+                        Console.WriteLine($"알겠습니다. 캐릭터의 이름은 {name} 입니다.");
+                        break;
+                    }
+                    else if (cho1 == 2)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("1이나 2만 입력해 주세요.");
+                        Thread.Sleep(1500);
+                        continue;
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("1이나 2만 입력해 주세요.");
+                    Console.WriteLine("숫자로 입력해 주세요.");
                     Thread.Sleep(1500);
-                    Main();
+                    continue;
                 }
             }
-            else
-            {
-                Console.WriteLine("숫자로 입력해 주세요.");
-                Main();
-            }
-        }
-        
 
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("직업을 선택해 주십시오.");
+
+            }
+            // 이후 게임 진행 로직
+            Console.WriteLine("게임을 시작합니다...");
+        }
     }
 }
